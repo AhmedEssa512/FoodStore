de0f163a1b5e3a2fb22cc55d2174c74c35a37153
Ahmed Esaa <ahmedelsaiyed2gmail.com>
2024-08-12 00:53:34 +0200

    CategoryTest Preparation

---
+++ FoodStore.XUnitTest/CategoryControllerTests.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+using FoodStore.Api.Controllers;
+using FoodStore.Data.Entities;
+using FoodStore.Service.Abstracts;
+using FoodStore.Service.Authorization;
+using FoodStore.Service.Context;
+using FoodStore.Service.Implementations;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+
+namespace FoodStore.XUnitTest;
+
+public class CategoryControllerTests
+{
+}
