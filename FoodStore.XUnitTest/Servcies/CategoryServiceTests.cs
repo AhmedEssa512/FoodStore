4b7376a51cbb5771fef81095f6af4d721e27e964
Ahmed Esaa <ahmedelsaiyed2@gmail.com>
2025-02-20 21:54:07 +0200

    Add category tests

--- FoodStore.XUnitTest/Servcies/CategoryServiceTests.cs
+++ FoodStore.XUnitTest/Servcies/CategoryServiceTests.cs
@@ -2,19 +2,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FoodStore.Data.DTOS;
+using FoodStore.Data.Entities;
+using FoodStore.Service.Implementations;
+using FoodStore.Service.IRepository;
+
+
 
 namespace FoodStore.XUnitTest.Servcies
 {
     public class CategoryServiceTests
     {
-        [Fact]
-        public void TestName()
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly Mock<ICategoryRepository> _mockCategoryRepository;
+        private readonly CategoryService _service;
+
+
+        public CategoryServiceTests()
         {
-            // Arrange
-        
-            // Act
-        
-            // Assert
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _service = new CategoryService(_mockUnitOfWork.Object);
+            _mockCategoryRepository = new Mock<ICategoryRepository>();
+            _mockUnitOfWork.Setup(u => u.Category).Returns(_mockCategoryRepository.Object);
         }
+
+    [Fact]
+    public async Task AddCategoryAsync_ValidCategory_AddsCategory()
+    {
+        // Arrange
+        var categoryDto = new CategoryDto
+        {
+            Name = "Electronics",
+            Description = "Various electronic items"
+        };
+
+        // Act
+        await _service.AddCategoryAsync(categoryDto);
+
+        // Assert
+        _mockCategoryRepository.Verify(repo => repo.AddAsync(It.Is<Category>(c => c.Name == categoryDto.Name && c.Description == categoryDto.Description)), Times.Once);
+        _mockUnitOfWork.Verify(uow => uow.SaveChangesAsync(), Times.Once);
+    }
+    
     }
 }
